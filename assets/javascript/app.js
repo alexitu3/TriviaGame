db86f7b2377a2971dd0e05c069c665978b763bb4
alex <alex@calvisitor-10-105-173-248.calvisitor.1918.berkeley.edu>
2017-09-26 20:41:39 -0700

    structure

---
+++ assets/javascript/app.js
@@ -0,0 +1,51 @@
+var timer = 120
+
+
+var gameOper = {
+
+    qA : {Q1 : "nvjbhj", a1 : "klajsd", a2 : "asdf", a3: "sdfg", a4 : "werg"  },
+          {Q2 : "asdf", a1 : "klajsd", a2 : "asdf", a3 : "sdfg", a4 : "werg"  },
+          {Q3 : "asdf", a1 : "klajsd", a2 : "asdf", a3 : "sdfg", a4 : "werg"  },
+          {Q4 : "asdf", a1 : "klajsd", a2 : "asdf", a3 : "sdfg", a4 : "werg"  },
+          {Q5 : "asdf", a1 : "klajsd", a2 : "asdf", a3 : "sdfg", a4 : "werg"  }
+    
+    timer : someFunction()
+
+
+};
+
+
+function start() {
+
+  $("btn").on("click")
+
+
+}
+
+function countdown() {
+  var interval = setIntravel(decrament, 1000);
+}
+
+function decrament() {
+  number--;
+  if(number === 0) {
+    end();
+
+  }
+}
+
+
+function end() {
+  // show # of correct/wrong (or not) answered
+
+  // say game over
+  $("#gameover").html("Game over!!!")
+}
+
+
+
+
+
+// where in the world is carman san diego?
+// why did the chicken cross the road?
+// 
