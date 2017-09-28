98dc22ab7fbfe7a95de6b23b4f98d13685c3bfa1
alex <alex@alexs-MacBook-Pro.local>
2017-09-27 19:20:44 -0700

    another day

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,45 @@
-var timer = 120
 
 
-var gameOper = {
-
-    qA : {Q1 : "nvjbhj", a1 : "klajsd", a2 : "asdf", a3: "sdfg", a4 : "werg"  },
-          {Q2 : "asdf", a1 : "klajsd", a2 : "asdf", a3 : "sdfg", a4 : "werg"  },
-          {Q3 : "asdf", a1 : "klajsd", a2 : "asdf", a3 : "sdfg", a4 : "werg"  },
-          {Q4 : "asdf", a1 : "klajsd", a2 : "asdf", a3 : "sdfg", a4 : "werg"  },
-          {Q5 : "asdf", a1 : "klajsd", a2 : "asdf", a3 : "sdfg", a4 : "werg"  }
+var number = 120
     
+    var QA = {
 
-};
 
+      Q1 : { 
+          Q : "Where in the world is Carmen San Diego?",
+          A : ["nvjbhj", a1 : "klajsd", a2 : "asdf", a3: "sdfg", a4 : "werg"]
+           
+           },
+
+      Q2 : {
+              Q : "why did the chicken cross the road?",
+              A : ["asdf", a1 : "klajsd", a2 : "asdf", a3 : "sdfg", a4 : "werg"]
+
+             },   
+
+      Q3 : { 
+               Q : "asdfasd",
+               A :  ["asdf", a1 : "klajsd", a2 : "asdf", a3 : "sdfg", a4 : "werg"]
+             
+            },
+
+      Q4 : { 
+              Q : "asdf",
+              A : ["asdf", a1 : "klajsd", a2 : "asdf", a3 : "sdfg", a4 : "werg"]
+            
+            },
+
+      Q5 : {    
+              Q : "lkj"
+              A : ["asdf", a1 : "klajsd", a2 : "asdf", a3 : "sdfg", a4 : "werg"] 
+        
+        } 
+    };
 
 function start() {
 
   $("#button").on("click")
-
+  
 
 };
 
@@ -26,7 +49,9 @@ function countdown() {
 
 function decrament() {
   number--;
+  $("#show-number").html("<h2>" + number + "</h2>");
   if(number === 0) {
+  
     end();
 
   }
@@ -40,10 +65,17 @@ function end() {
   $("#gameover").html("Game over!!!")
 }
 
+// still need to work on the render function.... 
+function renderQA() {
+  for(var i = 0; i <= 5; i++) {
+    $(":radio").append("<div>")
+  }
+}
+
+countdown();
 
 
 
-
-// where in the world is carman san diego?
+// where in the world is carman san diego? A: Normandy, Taipei, Kulam
 // why did the chicken cross the road?
 // 
